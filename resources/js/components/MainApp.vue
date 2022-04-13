86424ef7a40ed5cc062e388b82048cee9e4fd0a4
Nestor45 <nestoralvarez97@gmail.com>
2022-04-13 14:55:01 -0500

    creacion de vistas vue (Home-Hearder)

---
+++ resources/js/components/MainApp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="main">
+        <Hearder/>
+        <div class="content">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Hearder from './Header.vue'
+    export default{
+        name: 'main-app',
+        components: {
+            Hearder,
+        }
+    }
+</script>
